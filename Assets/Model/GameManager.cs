547933c4ac2b75e253a2d71f740d7620c347fcc0
riku <riku2004@gmail.com>
2024-10-26 14:00:47 +0900

    スコアクラスの追加

--- Assets/Model/GameManager.cs
+++ Assets/Model/GameManager.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public Timer timer;
+    public Score score;
     // Start is called before the first frame update
     void Start()
     {
         timer = new Timer();
         timer.StartTimer();
+        score = new Score();
+        Debug.Log(score.ToString());
+        score.AddPoints(10);
+        Debug.Log(score.ToString());
+        score.ResetScore();
+        Debug.Log(score.ToString());
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(timer.GetCurrentTime());
+        // Debug.Log(timer.GetCurrentTime());
     }
 }
