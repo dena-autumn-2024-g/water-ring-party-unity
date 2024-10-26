3fa1db4c63fe546fdd89be2598412c0b5657c421
riku <riku2004@gmail.com>
2024-10-26 13:32:44 +0900

    タイマークラスを追加

--- Assets/Model/GameManager.cs
+++ Assets/Model/GameManager.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public Timer timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = new Timer();
+        timer.StartTimer();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(timer.GetCurrentTime());
     }
 }
