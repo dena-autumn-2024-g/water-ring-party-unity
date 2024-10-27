4a3e758d83b39c843d7c1ea966350424ee38f41f
riku <riku2004@gmail.com>
2024-10-27 11:47:50 +0900

    プレイヤークラスとキャノンクラスの追加

---
+++ Assets/Model/Canon.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+class Canon {
+    public Vector3 power = Vector3.zero;
+    public int id;
+    
+    public Canon(int canonId) {
+        this.id = canonId;
+    }
+
+    public void setPower(Vector3 power) {
+        this.power = power;
+    }
+}
--- Assets/Model/Player.cs
+++ Assets/Model/Player.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Player : MonoBehaviour
+class Player
 {
     // 砲台の状態を管理する変数
     private bool isPushButton = false;
@@ -8,16 +8,25 @@ public class Player : MonoBehaviour
     // 現在選択されている砲台番号（0-4）
     private int currentTurretNumber = 0;
 
+    // プレイヤーのID(0 - 9)
+    private int playerId;
 
+    public Player(int id) {
+        isPushButton = false;
+        currentTurretNumber = 0;
+        this.playerId = id;
+    }
     // 砲台番号を切り替える関数
     public void switchTurretNumber(int newNumber)
     {
         currentTurretNumber = (currentTurretNumber + newNumber) % 5;
+        Debug.Log("switchTurretNumber: " + currentTurretNumber);
     }
 
     // 砲台のオン・オフを切り替える関数
     public void setTurretState(bool state)
     {
+        Debug.Log("setTurretState: " + state);
         isPushButton = state;
     }
 
@@ -31,5 +40,9 @@ public class Player : MonoBehaviour
     public int GetCurrentTurretNumber()
     {
         return currentTurretNumber;
+    }
+
+    public int GetPlayerId() {
+        return playerId;
     }
 }
