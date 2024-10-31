134d5fd91fe106f3bec09bdbc465c22a932477e6
Yuma <bird.torichan@gmail.com>
2024-11-01 05:28:40 +0900

    static変数がEditor再生時に初期化されるようにCommonInfoInitializerを追加

---
+++ Assets/Model/CommonInfoInitializer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CommonInfoInitializer : MonoBehaviour
+{
+    private static CommonInfoInitializer _instance;
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        _instance = this;
+
+        DontDestroyOnLoad(gameObject);
+        InitializeCommonInfoManager();
+    }
+
+    private void InitializeCommonInfoManager()
+    {
+        CommonInfoManager.ROOM_ID = "";
+        CommonInfoManager.NUM_PLAYER = 0;
+        CommonInfoManager.SCORE_LEFT_TIME = 0;
+        CommonInfoManager.SCORE_RING_COUNT = 0;
+        CommonInfoManager.SCORE_POINT = 0;
+        CommonInfoManager.END_GAME = false;
+    }
+}
