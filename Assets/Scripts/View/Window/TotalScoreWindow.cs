2fee08884a994a58fbf6c37a4b05ea69be75b542
YukaChoco <sugar.ore.yk@gmail.com>
2024-11-07 01:29:08 +0900

    スコア頑張って見てたやつ

--- Assets/Scripts/View/Window/TotalScoreWindow.cs
+++ Assets/Scripts/View/Window/TotalScoreWindow.cs
@@ -9,8 +9,46 @@ public class TotalScoreWindow : MonoBehaviour
 
     void Start()
     {
-        //int score = CommonInfoManager.SCORE_RING_COUNT;
-        //scoreText.text = score.ToString() + "点!";
+        int[] scores = CommonInfoManager.SCORE_RING_COUNT;
+        int maxScore = 0;
+        // 優勝者のindexのリスト
+        List<int> winnerList = new List<int>();
+
+        for (int i = 1; i < 10; i++)
+        {
+            if (scores[i] > maxScore)
+            {
+                maxScore = scores[i];
+                winnerList.Clear();
+                winnerList.Add(i);
+            }
+            else if (scores[i] == maxScore)
+            {
+                winnerList.Add(i);
+            }
+        }
+
+        string displayText = "";
+        bool isDraw = winnerList.Count === CommonInfoManager.NUM_PLAYER || winnerList.Count === 0;
+
+        if (isDraw)
+        {
+            displayText += "引き分け";
+        }
+        else
+        {
+            displayText += "優勝：Player";
+            for (int i = 0; i < winnerList.Count; i++)
+            {
+                displayText += (winnerList[i] + 1).ToString();
+                if (i < winnerList.Count - 1)
+                {
+                    displayText += ",";
+                }
+            }
+        }
+
+        scoreText.text = displayText;
     }
 
 }
